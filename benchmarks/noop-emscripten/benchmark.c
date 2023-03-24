faf52aac997eb2a622787a5e49b402ba185455eb
William_S <wsta@mechakhan.com>
2023-03-24 10:42:47 +0100

    remove large wasm files

---
+++ benchmarks/noop-emscripten/benchmark.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "wizer.h"
+#include "sightglass.h"
+
+bool initialized = false;
+
+static void init_func()
+{
+    initialized = true;
+}
+
+WIZER_INIT(init_func);
+
+int main()
+{
+    printf("[noop] calls bench_start and bench_end with no intervening code\n");
+    bench_start();
+    if (!initialized)
+        init_func();
+    bench_end();
+    printf("[noop] complete\n");
+}
